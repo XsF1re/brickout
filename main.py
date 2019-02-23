680dcd6b3426540264e3add0408509b2c929bce2
umi <bo314@naver.com>
2019-02-23 21:31:03 +0900

    modified the part of diagonal refectiongit add main.py! and added some comments I don't think I need the rainbow function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.gfxdraw
+import pygame.freetype
 import time
 import random
 import sys
@@ -29,16 +30,17 @@ class drawRainbow:
 pygame.init()
 
 pygame.display.set_caption("break Bricks!")
-screen=pygame.display.set_mode((850, 900))
+screen=pygame.display.set_mode((900, 600))
 clock=pygame.time.Clock()
 
 LOGO_IMAGE=pygame.image.load(os.path.join("logo.png"))
 MAP_WIDTH=500
 MAP_HEIGHT=500
-GAME_AREA=(100,50,100+MAP_WIDTH, 50+MAP_HEIGHT)
+GAME_AREA=(30,30,30+MAP_WIDTH, 30+MAP_HEIGHT)
 #게임 데이터들
-FPS=50
-FastFPS=True
+FPS=40
+FAST_FPS=40
+SLOW_FPS=20
 BAR_WIDTH=200
 BAR_HEIGHT=20
 BAR_MOVE_SPEED=20
@@ -46,33 +48,31 @@ BALL_MOVE_SPEED=4
 game=True
 barCoord=[0,MAP_HEIGHT-BAR_HEIGHT]
 boundary=False  #끝에 닿았는지
-ball=Ball((300,300), 10)
+ball=Ball((90,300), 10)
 RANDOM_VECTOR_SPEED=90
 randomVectorCount=1;
-vectorDirection=[3,3]
+vectorDirection=[1,-3]
 drawRainbow=drawRainbow(FPS)
 
 # to create bricks and brickList
 brickList={}
 
+#초기 bricks 생성
 for i in range(6):
-    brickList[i]=Brick.Brick((Brick.Brick.WIDTH, Brick.Brick.HEIGHT*(i+1)), 3)
-    brickList[i+6]=Brick.Brick((Brick.Brick.WIDTH*3, Brick.Brick.HEIGHT*(i+1)), 3)
-    brickList[i+12]=Brick.Brick((Brick.Brick.WIDTH*5, Brick.Brick.HEIGHT*(i+1)), 3)
+    brickList[i]=Brick.Brick((120, Brick.Brick.HEIGHT*i), 3)
+    brickList[i+6]=Brick.Brick((300, Brick.Brick.HEIGHT*i), 3)
 
-# 여기부터는 main과 비슷
-
-    # check if the ball crashes something
-    crashX=False
-    crashXSign="negative" # 음수벡터를 가져서 충돌 -> 양수로 바꿔줘야함
-    crashY=False
-    crashYSign="negative" # 음수벡터를 가져서 충돌 -> 양수로 바꿔줘야함
 while game:
+
+    # FPS를 이용해 진행 속도 설정
     clock.tick(FPS)
+
+    # 눌려있는 key 정보 받음
     keyState=pygame.key.get_pressed()
 
     if(keyState[pygame.K_RIGHT]):
         if(barCoord[0]>=MAP_WIDTH-BAR_WIDTH):
+            # 0으로 만들어주거나 MAP_WIDTH-BAR_WIDTH로 만들어 줘야 삐져나오지 않을 수 있음
             barCoord[0]=MAP_WIDTH-BAR_WIDTH
             boundary=True
         else :
@@ -85,6 +85,8 @@ while game:
         else :
             boundary=False
             barCoord[0]-=BAR_MOVE_SPEED
+
+    # pygame의 event를 받아봄
     for event in pygame.event.get():
 
         if (event.type==pygame.QUIT):
@@ -104,62 +106,38 @@ while game:
                     boundary=False
                     barCoord[0]-=BAR_MOVE_SPEED/2
             elif(event.key==pygame.K_f):
-                if(FastFPS==True):
-                    FastFPS=False
-                    FPS=40
+                # FAST와 SLOW FPS로 교대
+                if(FPS==FAST_FPS):
+                    FPS=SLOW_FPS
                 else:
-                    FastFPS=True
-                    FPS=5
+                    FPS=FAST_FPS
+
+            # vectorDirection이 (0,1) or (1,0) 등으로 인해
+            # 게임이 진행 안 되는 걸 막기 위함
             elif(event.key==pygame.K_a):
-                print ("vector adjusted")
-                print(vectorDirection)
                 c=0
                 while(c<=10):   #일단 10번만
                     vectorDirection[0]=random.randint(-10,10)
                     vectorDirection[1]=random.randint(-3,3)
                     if(vectorDirection[0]==0 or vectorDirection[1]==0):c-=1
                     c+=1
-                print(vectorDirection)
-    #SPEED만큼 count가 차면 랜덤벡터생성
-    # 잠시 random vector cease
-    # if(randomVectorCount%RANDOM_VECTOR_SPEED==0):
-    #     vectorDirection=[vectorDirection[0]+random.random()*4-2, vectorDirection[1]+random.random()*4-2]
-    #     print("random vector!!!")
-    #     randomVectorCount=0
-    # randomVectorCount+=1
-
-    # check out if the ball crashes map
 
     if(ball.coord[0]<=0+ball.radius):
-        # print(vectorDirection)
         if(vectorDirection[0]<0):
-
-            #print("-Wall and ball!")
             vectorDirection[0]*=(-1)
 
     elif(ball.coord[0]>=MAP_WIDTH-ball.radius):
         ball.coord[0]=MAP_WIDTH-ball.radius
-        # print(vectorDirection)
         if(vectorDirection[0]>0):
-            #print("+Wall and ball!")
             vectorDirection[0]*=(-1)
     if(ball.coord[1]<=0+ball.radius):
-        print("Wall and ball!")
-        print(vectorDirection)
         if(vectorDirection[1]<0):
             vectorDirection[1]*=(-1)
-
-    #check out if the ball crashes the bar
-    # if((ball.coord[0]+ball.radius>=barCoord[0] and ball.coord[0]-ball.radius<=barCoord[0]+BAR_WIDTH) and (ball.coord[1]+ball.radius>=barCoord[1])):
-    #     crashY=True
-    #     crashYSign="positive"
-
-    # ball의 radius는 현재 10으로 설정했음
-    #check out if the ball crashes any brick
     else:
         for i in brickList:
             brick=brickList[i]
-
+            # IMPORTANT!
+            # 이 if 문은 순수한 방향 (대각선 제외)
             # 완전 왼쪽에서오는 것
             if(ball.coord[0]>=brick.topLeft[0]-ball.radius and
             ball.coord[0]<=brick.topLeft[0] and
@@ -169,8 +147,7 @@ while game:
                 # hitBrick이 True를 리턴하면 부셔진 것임
                 if(brickList[i].hitBrick()):del brickList[i]
                 print(1)
-                crashX=True
-                crashXSign="positive"
+                vectorDirection[0]*=(-1)
                 break
 
             # 완전 오른쪽에서 오는 것
@@ -182,8 +159,7 @@ while game:
                 # hitBrick이 True를 리턴하면 부셔진 것임
                 if(brickList[i].hitBrick()):del brickList[i]
                 print(2)
-                crashX=True
-                crashXSign="negative"
+                vectorDirection[0]*=(-1)
                 break
             # 아래에서 오는 것
             elif(ball.coord[0]>=brick.bottomLeft[0]and
@@ -194,8 +170,7 @@ while game:
                 # hitBrick이 True를 리턴하면 부셔진 것임
                 if(brickList[i].hitBrick()):del brickList[i]
                 print(3)
-                crashY=True
-                crashYSign="negative"
+                vectorDirection[1]*=(-1)
                 break
             # 위에서 오는 것
             elif(ball.coord[0]>=brick.topLeft[0]and
@@ -206,112 +181,84 @@ while game:
                 # hitBrick이 True를 리턴하면 부셔진 것임
                 if(brickList[i].hitBrick()):del brickList[i]
                 print(4)
-                crashY=True
-                crashYSign="positive"
+                vectorDirection[1]*=(-1)
                 break
 
-            #벡터로 튕김각 계산하는 부분 시작not  중요함 진짜 ㅜㅜ
-
-            # 왼쪽 대각선 위에서 오는 것not  중요
-            # 우선은 적당한 범위 안에 있는 지 확인
-            # 드디어 구현했당.... ㅜㅅㅜ
-            elif(ball.coord[0]>=brick.topLeft[0]-ball.radius and
-            ball.coord[0]<=brick.topLeft[0] and
-            ball.coord[1]>=brick.topLeft[1]-ball.radius and
-            ball.coord[1]<=brick.topLeft[1] and not crashX and not crashY):
-                # hitBrick이 True를 리턴하면 부셔진 것임
-                if(brickList[i].hitBrick()):del brickList[i]
-                print(5)
-
-                # ball의 adjustVector 메소드를 빌림
-                tempBallVector=ball.adjustVector(vectorDirection)
-                tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topLeft[0], ball.coord[1]-brick.topLeft[1]])
-                vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
-                break
-            # 오른쪽 대각선 위에서 오는 것
-            elif(ball.coord[0]>=brick.topRight[0] and
-            ball.coord[0]<=brick.topRight[0]+ball.radius and
-            ball.coord[1]>=brick.topRight[1]-ball.radius and
-            ball.coord[1]<=brick.topRight[1] and not crashX and not crashY):
-                # hitBrick이 True를 리턴하면 부셔진 것임
-                if(brickList[i].hitBrick()):del brickList[i]
-                print(6)
-
-                # ball의 adjustVector 메소드를 빌림
-                tempBallVector=ball.adjustVector(vectorDirection)
-                tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topLeft[0], ball.coord[1]-brick.topLeft[1]])
-                vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
-                break
-
-            # 왼쪽 대각선 아래에서 오는 것
-            elif(ball.coord[0]>=brick.bottomLeft[0]-ball.radius and
-            ball.coord[0]<=brick.bottomLeft[0] and
-            ball.coord[1]>=brick.bottomLeft[1]and
-            ball.coord[1]<=brick.bottomLeft[1]+ball.radius and not crashX and not crashY ):
-                # hitBrick이 True를 리턴하면 부셔진 것임
-                if(brickList[i].hitBrick()):del brickList[i]
-                print(7)
-
-                # ball의 adjustVector 메소드를 빌림
-                tempBallVector=ball.adjustVector(vectorDirection)
-                tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topLeft[0], ball.coord[1]-brick.topLeft[1]])
-                vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
-                break
-            # 오른쪽 대각선 아래에서 오는 것
-            elif(ball.coord[0]>=brick.bottomRight[0] and
-            ball.coord[0]<=brick.bottomRight[0]+ball.radius and
-            ball.coord[1]>=brick.bottomRight[1] and
-            ball.coord[1]<=brick.bottomRight[1]+ball.radius and not crashX and not crashY):
-                # hitBrick이 True를 리턴하면 부셔진 것임
-                if(brickList[i].hitBrick()):del brickList[i]
-                print(8)
-
-                # ball의 adjustVector 메소드를 빌림
-                tempBallVector=ball.adjustVector(vectorDirection)
-                tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topLeft[0], ball.coord[1]-brick.topLeft[1]])
-                vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
-                break
+            # IMPORTANT
+            # 이 else문은 순수하지 않은 방향( 즉 대각선 )이나 그 밖에 있을 때
             else:
-                crashX=False
-                crashY=False
+                #벡터로 튕김각 계산하는 부분 시작not  중요함 진짜 ㅜㅜ
 
-    # X좌표가 무엇인가와 충돌했는데
-    if(crashX):
-        print("X crashed")
-        if(crashXSign=="negative"):
-            if(vectorDirection[0]<0):
-                vectorDirection[0]*=(-1)
-                #randomVectorCount=1 #시간 초기화
-        if(crashXSign=="positive"):
-            if(vectorDirection[0]>0):
-                vectorDirection[0]*=(-1)
-                #randomVectorCount=1
-            vectorDirection
+                # 왼쪽 대각선 위에서 오는 것not  중요
+                # 우선은 적당한 범위 안에 있는 지 확인
+                # 드디어 구현했당.... ㅜㅅㅜ
+                if(ball.coord[0]>=brick.topLeft[0]-ball.radius and
+                ball.coord[0]<=brick.topLeft[0] and
+                ball.coord[1]>=brick.topLeft[1]-ball.radius and
+                ball.coord[1]<=brick.topLeft[1]):
+                    # hitBrick이 True를 리턴하면 부셔진 것임
+                    if(brickList[i].hitBrick()):del brickList[i]
+                    print(5)
 
-    # y좌표가 무엇인가와 충돌했는데
-    if(crashY):
-        print("Y crashed")
-        if(crashYSign=="negative"):
-            if(vectorDirection[1]<0):
-                vectorDirection[1]*=(-1)
-                # randomVectorCount=1
-        if(crashYSign=="positive"):
-            if(vectorDirection[1]>0):
-                vectorDirection[1]*=(-1)
-                # randomVectorCount=1
+                    # ball의 adjustVector 메소드를 빌림
+                    tempBallVector=ball.adjustVector(vectorDirection)
+                    tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topLeft[0], ball.coord[1]-brick.topLeft[1]])
+                    vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
+                    break
+                # 오른쪽 대각선 위에서 오는 것
+                elif(ball.coord[0]>=brick.topRight[0] and
+                ball.coord[0]<=brick.topRight[0]+ball.radius and
+                ball.coord[1]>=brick.topRight[1]-ball.radius and
+                ball.coord[1]<=brick.topRight[1]):
+                    # hitBrick이 True를 리턴하면 부셔진 것임
+                    if(brickList[i].hitBrick()):del brickList[i]
+                    print(6)
 
-    # bar과 충돌했을 때
+                    # ball의 adjustVector 메소드를 빌림
+                    tempBallVector=ball.adjustVector(vectorDirection)
+                    tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.topRight[0], ball.coord[1]-brick.topRight[1]])
+                    vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
+                    break
 
+                # 왼쪽 대각선 아래에서 오는 것
+                elif(ball.coord[0]>=brick.bottomLeft[0]-ball.radius and
+                ball.coord[0]<=brick.bottomLeft[0] and
+                ball.coord[1]>=brick.bottomLeft[1]and
+                ball.coord[1]<=brick.bottomLeft[1]+ball.radius):
+                    # hitBrick이 True를 리턴하면 부셔진 것임
+                    if(brickList[i].hitBrick()):del brickList[i]
+                    print(7)
+
+                    # ball의 adjustVector 메소드를 빌림
+                    tempBallVector=ball.adjustVector(vectorDirection)
+                    tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.bottomLeft[0], ball.coord[1]-brick.bottomLeft[1]])
+                    vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
+                    break
+                # 오른쪽 대각선 아래에서 오는 것
+                elif(ball.coord[0]>=brick.bottomRight[0] and
+                ball.coord[0]<=brick.bottomRight[0]+ball.radius and
+                ball.coord[1]>=brick.bottomRight[1] and
+                ball.coord[1]<=brick.bottomRight[1]+ball.radius):
+                    # hitBrick이 True를 리턴하면 부셔진 것임
+                    if(brickList[i].hitBrick()):del brickList[i]
+                    print(8)
+
+                    # ball의 adjustVector 메소드를 빌림
+                    tempBallVector=ball.adjustVector(vectorDirection)
+                    tempSlopeVecotr=ball.adjustVector([ball.coord[0]-brick.bottomRight[0], ball.coord[1]-brick.bottomRight[1]])
+                    vectorDirection=[float(float(tempBallVector[0])+float(tempSlopeVecotr[0])), float(float(tempBallVector[1])+float(tempSlopeVecotr[1]))]
+                    break
+
+    # bar과 ball이 충돌했을 때
     if(vectorDirection[1]>0 and (ball.coord[0]+ball.radius>=barCoord[0] and
     ball.coord[0]-ball.radius<=barCoord[0]+BAR_WIDTH and
     ball.coord[1]>=barCoord[1]-ball.radius and
     ball.coord[1]<=barCoord[1])):
-        print("crash bar")
         ball.coord[1]=barCoord[1]-ball.radius
         vectorDirection[1]*=(-1)
-        # randomVectorCount=1
 
     if(ball.coord[1]>MAP_HEIGHT): ball=Ball((300,300), 10)
+
     # DRAWING BEGINS
     screen.fill((255, 255, 255))
 
@@ -328,7 +275,21 @@ while game:
     else:
         # GAME_AREA=(100,50,100+MAP_WIDTH, 50+MAP_HEIGHT)
         pygame.draw.rect(screen, (0,200,200), (barCoord[0]++GAME_AREA[0], barCoord[1]++GAME_AREA[1], BAR_WIDTH, BAR_HEIGHT), 5)
-    screen.blit(LOGO_IMAGE, (650,100))
-    # pygame.draw.line(screen, drawRainbow.rgb, (650,140), (800,140), 15)
-    # drawRainbow.adjustRGB()
+    screen.blit(LOGO_IMAGE, (550,100))
+
+    #글자쓰기.. 까다로운듯
+    lineHeight=30
+    lineCount=0
+    def writeGuideText(text, x, y):
+        guideFont=pygame.freetype.Font("nanum.ttf", 20)
+        guidTextSurface=guideFont.render_to(screen, (x,y), text, (100,100,100))
+
+    writeGuideText("key 'A' means", 550, 200+lineHeight*lineCount)
+    lineCount+=1
+    writeGuideText("to adjust the direction of the ball", 550, 200+lineHeight*lineCount)
+    lineCount+=1
+    writeGuideText("key 'F' means", 550, 200+lineHeight*lineCount)
+    lineCount+=1
+    writeGuideText("to change FPS", 550, 200+lineHeight*lineCount)
+    
     pygame.display.flip()
